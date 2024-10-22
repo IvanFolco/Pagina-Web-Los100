18037e2c3c49878b624906c64fb87600427972a2
IvanFolco <112995132+IvanFolco@users.noreply.github.com>
2024-10-21 22:34:06 -0300

    Add files via upload

--- galeria/galeria.js
+++ galeria/galeria.js
@@ -1,16 +1,11 @@
-// Seleccionar el modal
 var modal = document.getElementById("modal");
 
-// Seleccionar la imagen dentro del modal
 var modalImg = document.getElementById("imagen-ampliada");
 
-// Seleccionar todas las imágenes de la galería
 var imagenes = document.querySelectorAll('.imagen-galeria');
 
-// Seleccionar el botón de cerrar
 var cerrar = document.getElementsByClassName("cerrar")[0];
 
-// Abrir el modal cuando se haga clic en una imagen
 imagenes.forEach(function(imagen) {
     imagen.onclick = function() {
         modal.style.display = "flex";
@@ -18,12 +13,10 @@ imagenes.forEach(function(imagen) {
     }
 });
 
-// Cerrar el modal cuando se haga clic en el botón de cerrar
 cerrar.onclick = function() {
     modal.style.display = "none";
 }
 
-// Cerrar el modal haciendo clic fuera de la imagen ampliada
 window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
--- main.js
+++ main.js
@@ -1,12 +1,12 @@
-// Definir la función toggleMenu
+
 function toggleMenu() {
-    // Selecciona el menú móvil
+    
     var menuMovil = document.querySelector('.menu-movil');
     
-    // Alternar la clase 'mostrar' para desplegar o esconder el menú
+    
     menuMovil.classList.toggle('mostrar');
 }
 
-// Añadir el evento onclick al botón hamburguesa directamente desde el JavaScript
+
 var hamburguesa = document.querySelector('.hamburguesa');
 hamburguesa.onclick = toggleMenu;
